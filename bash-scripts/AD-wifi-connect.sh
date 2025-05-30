9c19810ad93011c481c2bedb9c72ae5ee4e96eaf
Sanket Ghodnadikar <sankettg2000@gmail.com>
2025-05-30 09:14:09 +0530

    delete any old wifi conf is available and dont try to connect when local user is login.

--- bash-scripts/AD-wifi-connect.sh
+++ bash-scripts/AD-wifi-connect.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+nmcli connection delete KPIT-AD-USER
+if [ "$USERNAME" = "kpit" ]; then
+exit 0
+fi
 nmcli connection add type wifi con-name "KPIT-AD-USER" ssid "KPIT-AD-USER"
 nmcli c modify KPIT-AD-USER 802-1x.eap peap 802-1x.identity $user  802-1x.phase2-auth mschapv2
 nmcli con modify KPIT-AD-USER wifi-sec.key-mgmt wpa-eap
