c1e71964632593312386b3e9db5a85381f305084
Sanket Ghodnadikar <sankettg2000@gmail.com>
2025-05-30 15:22:31 +0530

    logic to connect wifi

--- bash-scripts/AD-wifi-connect.sh
+++ bash-scripts/AD-wifi-connect.sh
@@ -10,7 +10,22 @@ nmcli connection delete KPIT-AD-USER
 if [ "$USERNAME" = "kpit" ]; then
 exit 0
 fi
-run_command "nmcli connection add type wifi con-name "KPIT-AD-USER" ssid "KPIT-AD-USER""
-run_command "nmcli c modify KPIT-AD-USER 802-1x.eap peap 802-1x.identity $user  802-1x.phase2-auth mschapv2"
-run_command "nmcli con modify KPIT-AD-USER wifi-sec.key-mgmt wpa-eap"
-run_command "nmcli con up KPIT-AD-USER"
+function connect_wifi(){
+    run_command "nmcli connection add type wifi con-name "KPIT-AD-USER" ssid "KPIT-AD-USER""
+    run_command "nmcli c modify KPIT-AD-USER 802-1x.eap peap 802-1x.identity $user  802-1x.phase2-auth mschapv2"
+    run_command "nmcli con modify KPIT-AD-USER wifi-sec.key-mgmt wpa-eap"
+    run_command "nmcli con up KPIT-AD-USER"
+}
+
+kpit_wifi_status=$(nmcli device | grep 'connected\s\+\KPIT-AD-USER')
+other_wifi_status=$(nmcli device | grep 'wlp0s20f3\s\+\wifi\s\+\connected')
+available_wifi_status=$(nmcli device wifi | grep KPIT-AD-USER)
+echo $kpit_wifi_status
+echo $other_wifi_status
+echo $available_wifi_status
+if [ -n "$kpit_wifi_status" ] || [ -n "$other_wifi_status" ] || ! [ -n "$available_wifi_status" ]; then
+  echo "Wifi is already connected"
+else
+  echo "run script"
+  connect_wifi
+fi
